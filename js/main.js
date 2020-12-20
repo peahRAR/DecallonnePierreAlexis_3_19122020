4ce98f86adc7e4a847f15262b8f6b6ae393d4a69
peah <padecallonne@gmail.com>
2020-12-20 01:27:29 +0100

    probleme getElementbyClassName sur la fleche du header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// GESTION DES INCLUDES
+
 fetch("./html/header.html")
   .then(response => {
     return response.text()
@@ -13,3 +15,23 @@ fetch("./html/footer.html")
   .then(data => {
     document.querySelector("footer").innerHTML = data;
   });
+
+  // GESTION CONDITION HEADER
+
+  let index =false;
+  let path = window.location.pathname;
+  let page = path.split("/").pop();
+  
+  if (page == "index.html") {
+    index = true;
+  }
+
+
+    var elem = document.getElementsByClassName("return");
+    console.log(elem.classList);
+
+
+
+
+
+  console.log(index)
